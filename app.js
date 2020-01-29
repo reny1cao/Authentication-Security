7cef6b5bb8eb617fb77df61f9d14e0658b902261
h0vscat <aarontsa2@gmail.com>
2020-01-29 00:18:28 -0500

    cookie&sessions&passport

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -13,14 +14,40 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 
-mongoose.connect("mongodb+srv://new-user_0:Aa_647ron_db@cluster0-0bwlj.mongodb.net/userDB", {useNewUrlParser: true});
+app.use(session({
+    secret: "Thisisasecret.",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// mongoose.connect("mongodb+srv://new-user_0:Aa_647ron_db@cluster0-0bwlj.mongodb.net/userDB", {useNewUrlParser: true});
+mongoose
+.connect("mongodb+srv://new-user_0:Aa_647ron_db@cluster0-0bwlj.mongodb.net/userDB", {
+useUnifiedTopology: true,
+useNewUrlParser: true,
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log("DB Connection Error: ${err.message}");
+});
+
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -34,49 +61,49 @@ app.get("/login", (req, res) => {
     res.render("login");
 });
 
-app.post("/register", (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.render("login");
+    }
+});
 
-        newUser.save((err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        });
+app.get("/logout", (req, res) => {
+    req.logOut();
+    res.redirect("/");
+});
+
+app.post("/register", (req, res) => {
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
+            });
+        }
     });
 });
 
 app.post("/login", (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    User.findOne({email: username}, (err, foundUser) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, (err => {
         if (err) {
             console.log(err);
         } else {
-            bcrypt.compare(password, foundUser.password, (err, result) => {
-                if (result === true) {
-                    res.render("secrets");
-                } else {
-                    res.render("Wrong number");
-                }
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
             });
         }
-    });
-    // User.find({}, (err, userDatas) => {
-    //     let email = req.body.email;
-    //     let password = req.body.password;
-    //     userDatas.forEach(element => {
-    //         if (element.email == email && element.password == password) {
-    //             res.render("secrets");
-    //         }
-    //     });
-    // });
+    }));
 });
+
 
 app.listen(3000, () => {
     console.log("Server started at port 3000");
